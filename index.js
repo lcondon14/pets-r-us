0e405029cd296f380e87d34581f99ad64bd4fb4c
lcondon14 <122494018+lcondon14@users.noreply.github.com>
2023-06-29 21:44:19 -0500

    add week 5 and update

--- index.js
+++ index.js
@@ -7,15 +7,23 @@ Description: Index.js page for Pets-r-us
 
 
 
-
-
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect(CONN).then(() => {
+    console.log('Connection to MongoDB database was successful\n  If you see this message it means you were able to connect to your MongoDB Atlas cluster');
+}).catch(err => {
+    console.log('MongoDB Error: ' + err.message);
+})
+
+const Customer = require('./modules/customer');
 
 // Sets the views directory
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/site", express.static(path.join(__dirname, "public/stylesheets")));
 
 // Sets the view engine to EJS
 app.set('view engine', 'ejs');
@@ -36,13 +44,29 @@ app.get('/boarding', (req, res) => {
 app.get('/training', (req, res) => {
     res.render('training');
 });
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
 // Server
 const PORT = process.env.PORT || 3000;
 
+app.post('register', async (req, res) => {
+    const { customerId, email } = req.body;
+// Create new customer    
+    try {
+       await Customer.create({ customerId, email });
+
+// Send success response
+    res.redirect('/');
+    } catch (error) {
+// Handles errors
+    res.status(500).json({ message: 'Registration failed', error: error.message });        
+    }
+});
 
 
-
-
-
+const CONN = 'mongodb+srv://lmcondon:Gpqzfv7Ilqzp9AGl@customers.psbtw5y.mongodb.net/?retryWrites=true&w=majority';
 // Port 3000
 app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
